d255a99a0d2d1e5af5569c7fc80dd76842e27c8c
Kelly Li <keljr@umich.edu>
2024-11-18 16:25:07 -0500

    new changes

--- R/hw3_lm.R
+++ R/hw3_lm.R
@@ -30,7 +30,7 @@ hw3_lm <- function(formula, data){
 
   # Residuals
   yhat <- x %*% bhat
-  resid <- y - yhat
+  resid <- as.numeric(y - yhat)
   min_resid <- min(resid)
   oneq_resid <- quantile(resid, 0.25, names = FALSE)
   med_resid <- median(resid)
@@ -48,7 +48,7 @@ hw3_lm <- function(formula, data){
   t_stat <- c(bhat / se)
   p_val <- c(2 * (1 - pt(q = abs(t_stat), df = n - p)))
 
-  # Final Residuals
+  # Final residuals
   final_resid <- c(Min = min_resid, `1Q` = oneq_resid,
                    Median = med_resid, `3Q` = threeq_resid,
                    Max = max_resid)
@@ -62,11 +62,24 @@ hw3_lm <- function(formula, data){
   resid_se <- sqrt(sse / (n - p))
   df <- n - p
 
+  # Find multiple & adjusted R squared
+  ybar <- mean(y)
+  ssr <- sum((yhat - ybar)^2)
+  m_r2 <- ssr / (ssr + sse)
+  a_r2 <- 1 - ((1 - m_r2) * (n - 1) / (n - p))
+
+  # Find F-statistic
+  msr <- ssr / (p - 1)
+  mse <- sse / df
+  fstat <- msr / mse
+  f_p_val <- 1 - pf(fstat, df1 = p - 1, df2 = df)
+
   # Create a list to store data
-  model <- list(residuals = final_resid, coefficients = final_coef,
-                fitted.values = yhat,
-                residuals = resid, residual_se = resid_se, df = df,
-                call = match.call())
+  model <- list(residuals = resid, resid_sum = final_resid,
+                coefficients = final_coef, fitted.values = yhat,
+                residual_se = resid_se, df = df,
+                m_r2 = m_r2, a_r2 = a_r2, fstat = fstat,
+                f_p_val = f_p_val, call = match.call())
   class(model) <- "hw3_lm"
   return(model)
 }
@@ -84,11 +97,18 @@ print.hw3_lm <- function(x, ...) {
   print(x$call)
 
   cat("\nResiduals:\n")
-  print(x$residuals)
+  print(x$resid_sum)
 
   cat("\nCoefficients:\n")
   printCoefmat(x$coefficients, digits = 4, signif.stars = TRUE)
 
   cat("\nResidual standard error:", format(x$residual_se, digits = 4),
       "on", x$df, "degrees of freedom\n")
+
+  cat("Multiple R-squared: ", format(x$m_r2, digits = 4),
+      ",  Adjusted R-squared: ", format(x$a_r2, digits = 4))
+
+  cat("\nF-statistic:", format(x$fstat, digits = 4), "on",
+      ncol(x$coefficients) - 1, "and", x$df,
+      "DF,  p-value:", format(x$f_p_val, digits = 4), "\n")
 }
